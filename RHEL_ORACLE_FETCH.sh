23e01d50a03413b9e0f43cd7f2e3204f483850b7
TejeshKumarReddyC <tejeshchagantireddy@gmail.com>
2025-05-22 17:31:29 +0530

    V1.1

--- RHEL_ORACLE_FETCH.sh
+++ RHEL_ORACLE_FETCH.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 servers="
-IPs
+10.218.192.34
+10.218.192.33
 "
-user=""
-password=""
+user="decomuser"
+password="n0twest+Decom!inux"
 script_file="acsv.sh"
 output_file="ou.txt"
+rm NF comb*
 
 echo "" > "$output_file"
 for server in $servers; do
  # echo "" > "$output_file"
-  yes | sshpass -p "$password" scp -r -o StrictHostKeyChecking=no -o HostKeyAlgorithms=ssh-rsa "$script_file" "$user@$server":/home/$user/
+  yes | sshpass -p "$password" scp -r -o StrictHostKeyChecking=no -o HostKeyAlgorithms=rsa-sha2-512 "$script_file" "$user@$server":/home/$user/
   if [ $? -ne 0 ]
   then
-	  echo "ERROR"
-	  echo "$server" >> NF
-	  continue
+          echo "ERROR"
+          echo "$server" >> NF
+          continue
   fi
   #yes | sshpass -p "$password" ssh -o StrictHostKeyChecking=no "$user@$server" "sudo bash $script_file >>$output_file"
-  yes | sshpass -p "$password" ssh -t -t -o StrictHostKeyChecking=no -o HostKeyAlgorithms=ssh-rsa  "$user@$server" "sudo -S bash $script_file >>$output_file"
-  yes | sshpass -p "$password" scp -r -o StrictHostKeyChecking=no -o HostKeyAlgorithms=ssh-rsa "$user@$server":/home/$user/"$output_file" "${server}_$output_file"
+  yes | sshpass -p "$password" ssh -t -t -o StrictHostKeyChecking=no -o HostKeyAlgorithms=rsa-sha2-512 "$user@$server" "sudo -S bash $script_file >>$output_file"
+
+  yes | sshpass -p "$password" scp -r -o StrictHostKeyChecking=no -o HostKeyAlgorithms=rsa-sha2-512 "$user@$server":/home/$user/"$output_file" "a${server}_$output_file"
   echo "done for $server"
 done
 
 cat *_$output_file > "combined_$output_file"
 rm $output_file
-
-
